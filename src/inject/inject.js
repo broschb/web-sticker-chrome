9c54bc9e1ff5435a852244ba7b838a95e72016a7
Brandon Broschinsky <broschb@gmail.com>
2014-10-01 21:05:53 -0600

    plugin adding link

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,9 +7,13 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-    $("<div id='websticker'>Click Me!</div>").appendTo("body");
-    $("body").append("<div id='websticker'>Click Me!</div>");
-		$("#websticker").button().click(clickHandler);
+    var widget = document.createElement('div');
+		widget.id = 'web-sticker'
+		widget.innerHTML = '<a href="#">Install</a>';
+		widget.className = "button";
+		document.body.innerHTML += widget.innerHTML;
+		// document.body.appendChild(download_btn);
+    // $("body").append($(download_btn.innerHTML));
 		console.log("Hello. This message was sent from scripts/inject.js");
 	}
 	}, 10);
