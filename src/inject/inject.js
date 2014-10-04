602af915801e8509c78ba18ca644aa9828623075
Brandon Broschinsky <broschb@gmail.com>
2014-10-04 12:59:34 -0600

    added popup menu

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,34 +1,38 @@
-menu = "<ul id='web-sticker-menu'>"+
-       "<li>"+
-			 "<a href='#'>Menu 1</a>"+
-			 "<ul>"
-       "<li><a href='#'>Sub Menu 1</a></li>"+
-       "<li><a href='#'>Sub Menu 2</a></li>"+
-       "<li><a href='#'>Sub Menu 3</a></li>"+
-       "</ul>"
-			 "</li>"
-			 "</ul>";
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-    var widget = document.createElement('div');
-		widget.id = 'web-sticker'
-		widget.style.backgroundImage="url("+chrome.extension.getURL('icons/orange48.png')+")";
-		// widget.innerHTML = menu;
-    var _body = document.getElementsByTagName('body') [0];
-    _body.appendChild(widget);
-		// document.body.innerHTML += widget.outerHTML;
-    // console.log(document.body.innerHTML)
-		document.getElementById('web-sticker').addEventListener("click", clickHandler);
-		// submenu("web-sticker-menu");
+    buildMenu();
 	}
 	}, 10);
 });
 
 function clickHandler(){
-	alert(getSelectionText());
+	// alert(getSelectionText());
+}
+
+var buildMenu = function(){
+  var _body = document.getElementsByTagName('body') [0];
+
+  //menu dive
+  var menu_div = document.createElement('div');
+  menu_div.id = 'web-sticker-menu'
+  for(var i = 0; i<2; i++){
+    var div = document.createElement('div')
+    menu_div.appendChild(div)
+  }
+
+  //button div
+  var widget = document.createElement('div');
+  widget.id = 'web-sticker'
+  widget.style.backgroundImage="url("+chrome.extension.getURL('icons/orange48.png')+")";
+
+  //append to body
+  _body.appendChild(menu_div);
+  _body.appendChild(widget);
+
+  //listeners
+  document.getElementById('web-sticker').addEventListener("click", clickHandler);
 }
 
 var getSelectionText = function(){
