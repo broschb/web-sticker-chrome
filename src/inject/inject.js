bce7eeae055ae9137cf397adeab372b2f8f7cc10
“Brandon <broschb@gmail.com>
2014-10-04 11:10:06 -0600

    fixed bug with js on embedded sites

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,13 @@
+menu = "<ul id='web-sticker-menu'>"+
+       "<li>"+
+			 "<a href='#'>Menu 1</a>"+
+			 "<ul>"
+       "<li><a href='#'>Sub Menu 1</a></li>"+
+       "<li><a href='#'>Sub Menu 2</a></li>"+
+       "<li><a href='#'>Sub Menu 3</a></li>"+
+       "</ul>"
+			 "</li>"
+			 "</ul>";
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -6,8 +16,13 @@ chrome.extension.sendMessage({}, function(response) {
     var widget = document.createElement('div');
 		widget.id = 'web-sticker'
 		widget.style.backgroundImage="url("+chrome.extension.getURL('icons/orange48.png')+")";
-		document.body.innerHTML += widget.outerHTML;
+		// widget.innerHTML = menu;
+    var _body = document.getElementsByTagName('body') [0];
+    _body.appendChild(widget);
+		// document.body.innerHTML += widget.outerHTML;
+    // console.log(document.body.innerHTML)
 		document.getElementById('web-sticker').addEventListener("click", clickHandler);
+		// submenu("web-sticker-menu");
 	}
 	}, 10);
 });
@@ -16,12 +31,13 @@ function clickHandler(){
 	alert(getSelectionText());
 }
 
-function getSelectionText(){
+var getSelectionText = function(){
+   console.log("click")
     var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
+    // if (window.getSelection) {
+        // text = window.getSelection().toString();
+    // } else if (document.selection && document.selection.type != "Control") {
+        // text = document.selection.createRange().text;
+    // }
     return text;
 }
